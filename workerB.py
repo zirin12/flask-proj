dd4298b859327ddbdaab7350f2c6effd3f7f6a62
Rahul Pillai <lector213@gmail.com>
2019-11-07 00:51:47 +0530

    updates task status in db using task_id

--- workerB.py
+++ workerB.py
@@ -1,13 +1,34 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from celery import Celery
+from celery.result import AsyncResult
+from models import Task,db
 
 # Celery configuration
-CELERY_BROKER_URL = 'amqp://rabbitmq:rabbitmq@rabbit:5672/'
+CELERY_BROKER_URL = 'amqp://rahul:rahul123@localhost:5672/myvhost'
 CELERY_RESULT_BACKEND = 'amqp'
 
 #Initialize Celery
 celery = Celery('workerB',broker=CELERY_BROKER_URL,backend = CELERY_RESULT_BACKEND)
 
-@celery.task()
-def sub_nums(a,b):
-    return a-b
+def connect(uri):
+    """Connects to the database and return a session"""
 
+    uri = uri
+
+    con = sqlalchemy.create_engine(uri)
+
+    Session = sessionmaker(bind=con)
+    session = Session()
+
+    return con, session
+
+@celery.task()
+def update_db(task_id, uri):
+    con, session = connect(uri)
+    res = AsyncResult(task_id)
+    print(res.ready())
+    if res.ready():
+        task = session.query(Task).get(task_id)
+        task.processed = True
+        session.commit()
