f0bc69acf7289cff7e3815fa191aec34d9b357d5
Rahul Pillai <lector213@gmail.com>
2019-11-09 22:26:14 +0530

    changed delay to apply_async and assigned queues to tasks

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.sqlite3'
 db.init_app(app)
 
 @app.route("/add_task",methods=['GET', 'POST'])
-def add():
+def add_task():
     number = request.args.get('number')
-    result = add_task.delay(int(number))
+    number = int(number)
+    result = add_task.apply_async((number,),queue='workerA')
     task = Task(result.task_id,False)
     db.session.add(task)
     db.session.commit()
     return jsonify({'result':result.task_id}), 200
 
 @app.route("/task_status",methods=['GET', 'POST'])
-def subtract():
+def task_status():
     task_id = request.args.get('task_id')
-    update_db.delay(task_id,'sqlite:///tasks.sqlite3')
+    update_db.apply_async((task_id,'sqlite:///tasks.sqlite3',),queue='workerB')
     task = Task.query.get(task_id)
     #print(task.processed)
     return jsonify({'result': task.processed}), 200
@@ -31,4 +32,4 @@ def subtract():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+        app.run(host='0.0.0.0')
