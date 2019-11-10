411b04443a44ea0ece18a5d9574a043813ce3a1b
Rahul Pillai <lector213@gmail.com>
2019-11-11 03:14:37 +0530

    Changed the urls for broker and backend

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+    CELERY_BROKER_URL = 'redis://redis:6379/0'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
